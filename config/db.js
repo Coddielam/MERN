2c1ec764b2f2b35b750e4230ec584edb0d8ec498
coddielam <edlam711@gmail.com>
2020-08-24 17:48:00 -0400

    Set up database connection (check config folder)

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const config = require("config");
+const mongoose = require("mongoose");
+
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database Connected ...");
+  } catch (err) {
+    console.error(err.message);
+    // instruct node to terminate the process synchronously
+    // exit with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
